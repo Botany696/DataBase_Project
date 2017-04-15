826ac3a5dbd07569764a4959b99c6462bdf7e4e1
mary3000 <mary-fifa3000@mail.ru>
2017-04-15 19:40:48 +0300

    Add files via upload

---
+++ Create_table.sql
@@ -0,0 +1,32 @@
+CREATE TABLE Заказы(
+Заказ INT,
+Блюдо INT,
+Столик INT,
+Дата TIMESTAMP WITHOUT TIME ZONE
+); 
+
+CREATE TABLE Блюда (
+Блюдо INT UNIQUE,
+Ингредиенты TEXT,
+Цена MONEY,
+Наличие BOOLEAN,
+Скидка INT,
+Вегетарианское BOOLEAN
+);
+
+CREATE TYPE MyType AS ENUM('VIP', 'У окна', 'Зал');
+CREATE TABLE Бронирование (
+    Столик INT UNIQUE,
+    Тип MyType,
+    Места INT,
+    Доступен BOOLEAN,
+    Цена MONEY,
+    Дата TIMESTAMP,
+    Время INTERVAL
+);
+
+CREATE TABLE Доходы (
+Дата TIMESTAMP WITHOUT TIME ZONE,
+Доход MONEY,
+Состояние_счета MONEY
+);
